ec102e0da647c03a74e9dba4433325a7162655b8
Rotimi Olanrewaju <rolament4real@gmail.com>
2025-05-13 01:30:06 -0700

    I modified app.jsx and install axios and also create reset passowrd page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import ResetPassword from "./pages/ResetPassword.jsx";
 
 
 const App = () => {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+          <Route path="/" element={<HomePage />} />
+          <Route path="/reset-password/:token" element={<ResetPassword />} />
       </Routes>
     </div>
   );
