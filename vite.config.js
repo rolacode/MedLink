ed48d2756b1a98e08c01d067ce34ceb279ced798
Rotimi Olanrewaju <rolament4real@gmail.com>
2025-05-22 10:42:48 -0700

    update vite config and create postcss and tailwind configurations

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),],
-
+  plugins: [react()],
   server: {
-    host: '0.0.0.0',   // 👈 This line is the key fix
-    port: 5173         // Optional: set a specific port if needed
-  },
-});
+    host: '0.0.0.0', 
+    port: 5173       
+  }
+})
