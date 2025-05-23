b697d6b819dfb62403d57abc88618879a5dc3fe8
Rotimi Olanrewaju <rolament4real@gmail.com>
2025-05-23 10:21:25 -0700

    changes of files

--- src/api/Axios.js
+++ src/api/Axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { jwtDecode } from "jwt-decode";
 
 const API = axios.create({
-  baseURL: "http://0.0.0.0:5000/api",
+  baseURL: "http://localhost:5000/api",
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
